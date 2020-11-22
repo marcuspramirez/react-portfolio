5d037d287d660ae5e9088c7248e00807a5eeb848
marcuspramirez <@macusramirez@gmail.com>
2020-11-22 11:30:13 -0600

    deploying

--- src/pages/404.js
+++ src/pages/404.js
@@ -4,7 +4,7 @@ import React from 'react';
 const NotFoundPage = () => {
     return (
         <div>
-            <h2>404 Not Found!</h2>
+            <h2>404 Not Found! Helloooooooooo </h2>
             
         </div>
     );
